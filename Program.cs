8190525261d8628055b52e7f27109a1bb717fecb
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-04-11 07:24:21 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Низове_и_знаци
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            var b =char.Parse(Console.ReadLine());
+            var c =char.Parse(Console.ReadLine());
+            var f =char.Parse(Console.ReadLine());
+            string d = Console.ReadLine();
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}",a,b,c,f,d);
+        }
+    }
+}
